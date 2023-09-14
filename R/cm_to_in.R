009b3bfdce7bac4406bafeb9acbcb72f6d6d9a21
a-klarkowska <143771538+a-klarkowska@users.noreply.github.com>
2023-09-14 13:29:19 +0900

    commit 2

--- R/cm_to_in.R
+++ R/cm_to_in.R
@@ -1,3 +1,13 @@
+#' Calculate value in centimetres to inches
+#'
+#' @param x numerical
+#'
+#' @return numerical
+#' @export
+#'
+#' @examples
+#' x <- 10
+#' cm_to_in(x)
 cm_to_in <- function(x) {
   x * 2.54
 }
---
+++ tests/testthat/test-cm_to_in.R
@@ -0,0 +1,3 @@
+test_that("multiplication works", {
+  expect_equal(cm_to_in(93), 236.22)
+})
